8560bd884d953df3dc9fe92fcedaa15ab16e9cef
relja456 <93168552+relja456@users.noreply.github.com>
2023-12-09 20:01:22 +0100

    modeli i relacije
    kreirani modeli: User, File i Folder, takodje kreirane i relacije izmedju njih

---
+++ laravel/app/Models/Folder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Folder extends Model
+{
+    use HasFactory;
+
+    protected $table = 'folders';
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'name',
+        'parent_folder_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
+
+    public function parentFolder()
+    {
+        return $this->belongsTo(Folder::class, 'parent_folder_id');
+    }
+
+    public function childFolders()
+    {
+        return $this->hasMany(Folder::class, 'parent_folder_id');
+    }
+
+}
