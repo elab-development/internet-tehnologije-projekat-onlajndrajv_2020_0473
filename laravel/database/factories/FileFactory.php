6cd4369e4717c19c4bd04fbf57da43e86270d38a
relja456 <93168552+relja456@users.noreply.github.com>
2023-12-09 22:00:34 +0100

    fabrike i seed
    kreirane fabrike da bi se napravili random podaci u bazi

---
+++ laravel/database/factories/FileFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\File>
+ */
+class FileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $users = User::pluck('id')->toArray();
+        return [
+            'user_id'=> fake()->randomElement($users),
+            'name'=>fake()->title(),
+            'size'=>fake()->randomFloat(),
+            'mime_type'=>fake()->mimeType(),
+            'content'=>fake()->randomDigitNotZero(),
+        ];
+    }
+}
---
+++ laravel/database/factories/FolderFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Folder>
+ */
+class FolderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $users = User::pluck('id')->toArray();
+        return [
+            'user_id'=> fake()->randomElement($users),
+            'name'=>fake()->title(),
+        ];
+    }
+}
