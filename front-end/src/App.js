5b1f1e0d66cc1a6e89e7ae624dbca3d39e13de94
relja456 <93168552+relja456@users.noreply.github.com>
2024-08-12 01:34:18 +0200

    added src

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./NavBar";
+import AllFiles from "./AllFiles";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <AllFiles />
+    </div>
+  );
+}
+
+export default App;
